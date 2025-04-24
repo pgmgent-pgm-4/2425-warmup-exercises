3a6446fc5d179ca3556fb80643010e74f8a68af2
Karel De Smet <karel.de.smet@outlook.com>
2025-04-24 08:06:46 +0200

    Add new exercise assignment (lesson: 24/04/2025)

---
+++ pgm4-2425-week1/warmup-exercise-week1-oef2.js
@@ -0,0 +1,10 @@
+/* 
+Veelvoud van eigen index
+---------------
+Schrijf een functie die een array van gehele getallen accepteert als argument. 
+De functie moet een array terug geven van de getallen die een veelvoud zijn van hun index in de oorspronkelijke array.
+Bijvoorbeeld:
+
+[22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+[68, -1, 1, -7, 10, 10] => [-1, 10]
+*/
