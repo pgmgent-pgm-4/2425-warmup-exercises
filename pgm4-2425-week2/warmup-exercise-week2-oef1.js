02dd4130175b3da94c019fae1f2fa292e49606a8
Karel De Smet <karel.de.smet@outlook.com>
2025-04-29 08:49:04 +0200

    Update week 2 oef 1 with some examples

--- pgm4-2425-week2/warmup-exercise-week2-oef1.js
+++ pgm4-2425-week2/warmup-exercise-week2-oef1.js
@@ -5,4 +5,14 @@ Beide arrays zijn niet leeg en hebben dezelfde lengte.
 Het eerste array stelt de correcte oplossing voor, het tweede array het antwoord. 
 De functie moet de absolute score teruggeven o.b.v. deze puntentelling:
 Goed antwoord: +4 // Fout antwoord: -1 // Blanco: 0
+
+Bijvoorbeeld:
+	
+   Juiste antwoorden  |   Effectief antwoord  | Resultaat         
+ ---------------------|-----------------------|-----------
+ ["a", "a", "b", "b"]   ["a", "c", "b", "d"]  →     6
+ ["a", "a", "c", "b"]   ["a", "a", "b", "" ]  →     7
+ ["a", "a", "b", "c"]   ["a", "a", "b", "c"]  →     16
+ ["b", "c", "b", "a"]   ["" , "a", "a", "c"]  →     0
+ 
 */
