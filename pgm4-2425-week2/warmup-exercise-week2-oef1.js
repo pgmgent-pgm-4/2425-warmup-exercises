0451c81d6c29c1d483079d2c5f9dc3e9599044df
Karel De Smet <karel.de.smet@outlook.com>
2025-04-29 08:25:50 +0200

    Add assignment for week 2 oef 1

---
+++ pgm4-2425-week2/warmup-exercise-week2-oef1.js
@@ -0,0 +1,8 @@
+/* 
+Multiple choice verbetering:
+Schrijf een functie die 2 arrays accepteert als argumenten. 
+Beide arrays zijn niet leeg en hebben dezelfde lengte. 
+Het eerste array stelt de correcte oplossing voor, het tweede array het antwoord. 
+De functie moet de absolute score teruggeven o.b.v. deze puntentelling:
+Goed antwoord: +4 // Fout antwoord: -1 // Blanco: 0
+*/
