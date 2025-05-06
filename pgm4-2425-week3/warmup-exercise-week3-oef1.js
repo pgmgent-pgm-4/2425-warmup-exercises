fdd22974a450492e37b5e01e275dbdbd1b04da52
Karel De Smet <karel.de.smet@outlook.com>
2025-05-06 08:07:02 +0200

    Add assignment for week 3 oef 1

---
+++ pgm4-2425-week3/warmup-exercise-week3-oef1.js
@@ -0,0 +1,8 @@
+/*
+Schrijf een functie fetchChickenRecipes die data ophaalt van "Free Recipe API" (https://www.themealdb.com/api.php)
+Je haalt eerst alle gerechten op waarvan de eerste letter 'c' is. 
+Dat kan via dit endpoint: https://www.themealdb.com/api/json/v1/1/search.php?f=c
+Geef vervolgens enkel de gerechten terug waarvan de naam begint met 'Chicken'
+De return value moet dus gelijk zijn aan een array: ['Chicken Enchilada Casserole', 'Chicken Handi', ...]
+Ter info: op dit moment zijn er 13 recepten waarvan de naam begint met "Chicken"
+*/
